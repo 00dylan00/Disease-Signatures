a5d979018b0e7d56c2e10598b18cd549d5257abd
00dylan00 <dylandalton2021@gmail.com>
2023-12-18 11:29:10 +0100

    update

--- scripts/iLINCS.freeze.py
+++ scripts/iLINCS.freeze.py
@@ -107,6 +107,7 @@ def get_compounds():
     url = "http://www.ilincs.org/api/Compounds?"
     response = requests.get(url)
     if response.status_code == 200:
+        
         return response.json()
     else:
         print("Failed to retrieve compounds")
